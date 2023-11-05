7cc8555d67dda6754978dfd7ad0f5e2812f4d833
colombo0718 <colombo0718@gmail.com>
2023-11-05 14:44:38 +0800

    fighter can fly by blockly

--- mineBlock.js
+++ mineBlock.js
@@ -117,3 +117,114 @@ Blockly.JavaScript['empty_array'] = function (block) {
   var code = '[]';
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
+
+//-------------------------------------------
+
+Blockly.Blocks['setinterval'] = {
+  init: function() {
+  //   this.appendDummyInput()
+  //   .appendField("[ ]");
+  // this.setOutput(true, 'Array');
+  // this.setColour('#aa00aa');
+  // this.setTooltip("empry array");
+  // this.setHelpUrl("");
+    this.appendDummyInput()
+        .appendField("setInterval");
+    this.appendStatementInput("code")
+        .setCheck(null);
+    this.appendValueInput("time_interval")
+        .setCheck("Number")
+        .appendField("dt");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour('#00bb00');
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+javascript.javascriptGenerator.forBlock['setinterval'] = function(block, generator) {
+  var statements_code = generator.statementToCode(block, 'code');
+  var value_time_interval = generator.valueToCode(block, 'time_interval', javascript.Order.ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = 'setInterval(function(){'+statements_code+'},'+value_time_interval+')\n';
+  return code;
+};
+
+//-------------------------------------------
+
+Blockly.Blocks['voxel_char'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("voxel char");
+    this.setOutput(true, null);
+    this.setColour('#00bbbb');
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+javascript.javascriptGenerator.forBlock['voxel_char'] = function(block, generator) {
+  // TODO: Assemble javascript into code variable.
+  var code = 'anima';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, 99];
+};
+
+//-------------------------------------------
+
+Blockly.Blocks['change_three_obj'] = {
+  init: function() {
+    this.appendValueInput("three_obj")
+        .setCheck(null)
+        .appendField("change");
+    this.appendValueInput("variation")
+        .setCheck(null)
+        .appendField(new Blockly.FieldDropdown([["position","position"], ["rotation","rotation"], ["scale","scale"]]), "transform")
+        .appendField(new Blockly.FieldDropdown([["x","x"], ["y","y"], ["z","z"]]), "axis")
+        .appendField("by");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour('#bbbb00');
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+javascript.javascriptGenerator.forBlock['change_three_obj'] = function(block, generator) {
+  var value_three_obj = generator.valueToCode(block, 'three_obj', javascript.Order.ATOMIC);
+  var dropdown_transform = block.getFieldValue('transform');
+  var dropdown_axis = block.getFieldValue('axis');
+  var value_variation = generator.valueToCode(block, 'variation', javascript.Order.ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = value_three_obj+'.'+dropdown_transform+'.'+dropdown_axis+'+='+value_variation+'\n';
+  return code;
+};
+
+//-------------------------------------------
+
+Blockly.Blocks['remainder'] = {
+  init: function() {
+    this.appendValueInput("a")
+        .setCheck(null);
+    this.appendValueInput("b")
+        .setCheck(null)
+        .appendField("%");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour("#0000ff");
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+javascript.javascriptGenerator.forBlock['remainder'] = function(block, generator) {
+  var value_a = generator.valueToCode(block, 'a', javascript.Order.ATOMIC);
+  var value_b = generator.valueToCode(block, 'b', javascript.Order.ATOMIC);
+  // TODO: Assemble javascript into code variable.
+  var code = value_a+'%'+value_b;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, 99];
+};
